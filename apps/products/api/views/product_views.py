2360476cfb160633dfd064db44b743abf9671081
JuanM <juanvarco95@gmail.com>
2021-08-02 09:40:45 -0500

    Algunos cambios

--- apps/products/api/views/product_views.py
+++ apps/products/api/views/product_views.py
@@ -15,20 +15,15 @@ class ProductListCreateAPIView(generics.ListCreateAPIView):
             return Response({'message' : 'Producto creado correctamente!!!'}, status = status.HTTP_201_CREATED)
         return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
 
-class ProductRetrieveAPIView(generics.RetrieveAPIView):
+class ProductRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = ProductSerializer
 
-    def get_queryset(self):
-        return self.get_serializer().Meta.model.objects.filter(state = True)
+    def get_queryset(self, pk = None):
+        if pk is None:
+            return self.get_serializer().Meta.model.objects.filter(state = True)
+        else:
+            return self.get_serializer().Meta.model.objects.filter(id = pk, state = True).first()
 
-
-    
-class ProductDestroyAPIView(generics.DestroyAPIView):
-    serializer_class = ProductSerializer
-
-    def get_queryset(self):
-        return self.get_serializer().Meta.model.objects.filter(state = True)
-    
     def delete(self, request, pk = None):
         product = self.get_queryset().filter(id = pk).first()
         if product:
@@ -37,16 +32,23 @@ class ProductDestroyAPIView(generics.DestroyAPIView):
             return Response({'message' : 'Producto eliminado correctamente'}, status = status.HTTP_200_OK)
         return Response({'error' : 'No existe un Producto con estos datos'}, status = status.HTTP_400_BAD_REQUEST)
     
-class ProductUpdateAPIView(generics.UpdateAPIView):
-    serializer_class = ProductSerializer
-
-    def get_queryset(self):
-        return self.get_serializer().Meta.model.objects.filter(state = True)
-    
     def patch(self, request, pk = None):
-        product = self.get_queryset().filter(id = pk).first()
-        if product:
-            product_serializer = self.serializer_class(product)
+        if self.queryset(pk):
+            product_serializer = self.serializer_class(self.queryset(pk), data = request.data)
             return Response(product_serializer.data, status = status.HTTP_200_OK)
+
         return Response({'error': 'No existe un producto con esos datos!'}, status = status.HTTP_400_BAD_REQUEST)
+
+    def put(self, request, pk = None):
+        if self.queryset(pk):
+            product_serializer = self.serializer_class(self.queryset(pk), data = request.data)
+
+            if product_serializer.is_valid():
+                product_serializer.save()
+                return Response(product_serializer.data, status = status.HTTP_200_OK)
+            
+            return Response(product_serializer.errors, status = status.HTTP_400_BAD_REQUEST)
+    
+
+    
 
